7360b76fbc4105494deead42995857c1963ed635
22050335-Zhang Shoufeng <zhangshoufeng2077@outlook.com>
2023-08-03 14:00:39 +0800

    Completed Experiment4 counter, didn't finish the timer  since no FPGA

---
+++ Experiment4/adder_counter/top.cpp
@@ -0,0 +1,51 @@
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "obj_dir/Vtop.h"
+
+VerilatedContext* contextp = NULL;
+VerilatedVcdC* tfp = NULL;
+
+static Vtop* top;
+
+void step_and_dump_wave(){
+  top->eval();
+  contextp->timeInc(1);
+  tfp->dump(contextp->time());
+}
+
+void sim_init(){
+  contextp = new VerilatedContext;
+  tfp = new VerilatedVcdC;
+  top = new Vtop;
+  contextp->traceEverOn(true);
+  top->trace(tfp, 0);
+  tfp->open("dump.vcd");
+}
+
+void sim_exit(){
+  step_and_dump_wave();
+  tfp->close();
+}
+
+int main() {
+  sim_init();
+
+  top->clk = 0; top->cin = 0; top->rst = 0; step_and_dump_wave();
+  printf("clk = %d, cin = %d, rst = %d, q = %d\n", top->clk, top->cin, top->rst, top->q);
+  top->clk = 1; top->cin = 0; top->rst = 0; step_and_dump_wave();
+  printf("clk = %d, cin = %d, rst = %d, q = %d\n", top->clk, top->cin, top->rst, top->q);
+  top->clk = 0; top->cin = 0; top->rst = 0; step_and_dump_wave();
+  printf("clk = %d, cin = %d, rst = %d, q = %d\n", top->clk, top->cin, top->rst, top->q);
+  top->clk = 1; top->cin = 0; top->rst = 0; step_and_dump_wave();
+  printf("clk = %d, cin = %d, rst = %d, q = %d\n", top->clk, top->cin, top->rst, top->q);
+  top->clk = 0; top->cin = 1; top->rst = 0; step_and_dump_wave();
+  printf("clk = %d, cin = %d, rst = %d, q = %d\n", top->clk, top->cin, top->rst, top->q);
+  top->clk = 1; top->cin = 1; top->rst = 0; step_and_dump_wave();
+  printf("clk = %d, cin = %d, rst = %d, q = %d\n", top->clk, top->cin, top->rst, top->q);
+  top->clk = 0; top->cin = 0; top->rst = 1; step_and_dump_wave();
+  printf("clk = %d, cin = %d, rst = %d, q = %d\n", top->clk, top->cin, top->rst, top->q);
+  top->clk = 1; top->cin = 0; top->rst = 1; step_and_dump_wave();
+  printf("clk = %d, cin = %d, rst = %d, q = %d\n", top->clk, top->cin, top->rst, top->q);
+  
+  sim_exit();
+}
---
+++ Experiment4/adder_counter/top.v
@@ -0,0 +1,12 @@
+`default_nettype none
+module top(clk, cin, rst, q);
+    input wire clk, cin, rst;
+    output reg [2:0] q;
+/* verilator lint_off WIDTHEXPAND */
+    always@(posedge clk)
+    begin
+        if(rst == 1'b1) q <= 1'b0;
+        else q <= q + cin;
+    end
+/* verilator lint_on WIDTHEXPAND */
+endmodule
